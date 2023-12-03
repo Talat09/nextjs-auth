ee922983a5e592f846b3518fe61cccc1116a131f
Talat09 <talat181008312@gmail.com>
2023-12-03 22:58:45 +0600

    done create user api models and client side signup page

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,18 +1,48 @@
 "use client";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect } from "react";
 import { useRouter } from "next/navigation";
-// import { axios } from "axios";
+import toast from "react-hot-toast";
+import axios from "axios";
+
 export default function SignupPage() {
+  const router = useRouter();
   const [user, setUser] = React.useState({
     email: "",
     password: "",
     username: "",
   });
-  const onSignup = async () => {};
+  const [buttonDisabled, setButtonDisabled] = React.useState(false);
+  const [loading, setLoading] = React.useState(false);
+  const onSignup = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.post("/api/users/signup", user);
+      console.log("SignUp success", response.data);
+      router.push("/login");
+    } catch (error: any) {
+      console.log("signup failed", error.message);
+      toast.error(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+  useEffect(() => {
+    if (
+      user.email.length > 0 &&
+      user.password.length > 0 &&
+      user.username.length > 0
+    ) {
+      setButtonDisabled(false);
+    } else {
+      setButtonDisabled(true);
+    }
+  }, [user]);
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <h1 className="text-center text-5xl">Sign Up</h1>
+      <h1 className="text-center text-3xl">
+        {loading ? "Loading..." : "Signup"}
+      </h1>
       <hr />
       <label htmlFor="username">username</label>
       <input
@@ -45,7 +75,7 @@ export default function SignupPage() {
         className="p-2 px-8 border border-gray-300 mb-4 rounded-md mt-4 focus:outline-none focus:border-black"
         onClick={onSignup}
       >
-        Signup
+        {buttonDisabled ? "No signup" : "Signup"}
       </button>
       <p>
         Already have an account? Go to
