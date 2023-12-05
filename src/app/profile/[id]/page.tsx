32b8d0aea7536e68185c4d3a511181b986f593c3
Talat09 <talat181008312@gmail.com>
2023-12-05 17:28:37 +0600

    added me route and get data by id from profile page

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,11 +1,11 @@
 export default function userProfile({ params }: any) {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2 ">
-      <h1>Profile</h1>
+      <h1 className="text-4xl">Profile</h1>
       <hr />
-      <p className="text-4xl">
+      <p className="text-4xl my-8">
         Profile page
-        <span className="mx-2 p-2 rounded bg-green-500 text-white">
+        <span className="mx-2 p-2  rounded bg-green-500 text-white">
           {params.id}
         </span>
       </p>
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -2,9 +2,11 @@
 import axios from "axios";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 import toast from "react-hot-toast";
 export default function ProfilePage() {
   const router = useRouter();
+  const [data, setData] = useState("nothing");
   const logout = async () => {
     try {
       await axios.get("/api/users/logout");
@@ -15,17 +17,36 @@ export default function ProfilePage() {
       toast.error(error.message);
     }
   };
+  //get user details
+  const getUserDetails = async () => {
+    const res = await axios.get("/api/users/me");
+    console.log("from profile page:", res.data.data);
+    setData(res.data.data._id);
+  };
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2 ">
       <h1 className="text-3xl font-bold">Profile</h1>
       <hr />
       <p className="text-4xl">Profile page</p>
+      <h2 className=" p-3 mt-4 rounded bg-green-500 text-white">
+        {data === "nothing" ? (
+          "Nothing"
+        ) : (
+          <Link href={`/profile/${data}`}>{data}</Link>
+        )}
+      </h2>
       <hr />
       <button
         className="bg-green-500 mt-4 text-white font-bold px-3 py-2 rounded "
         onClick={logout}
       >
         Logout
+      </button>
+      <button
+        className="bg-green-500 mt-4 text-white font-bold px-3 py-2 rounded "
+        onClick={getUserDetails}
+      >
+        Get User Details
       </button>
     </div>
   );
